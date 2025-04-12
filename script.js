3c3378f7107e0587f34a83d713319584fb1e8950
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-12 21:58:03 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// let hasDrive = false;
+// const passTest = true;
+
+// if (passTest) hasDrive = true;
+// if (hasDrive) console.log('He can drive')
+
+// function logger (){
+//     console.log("My name is Rejuan");
+// }
+
+// logger();
+
+
+
+function fruitProcess(apples, oranges){
+    const juice = `This nice juice made of ${apples} apples & ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcess(2,3))
+console.log(fruitProcess(6,8))
