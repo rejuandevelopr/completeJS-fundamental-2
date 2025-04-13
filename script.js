d6c560bd4141af6173f69099db919d4edab1b7d6
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-13 22:01:53 +0600

    3rd

--- script.js
+++ script.js
@@ -32,9 +32,55 @@ console.log(fruitProcess(2,2));
 
 
 
-function calAge1(birthyear){
-    const age = 2030 - birthyear;
-    return age;
+// function calAge1(birthyear){
+//     const age = 2030 - birthyear;
+//     return age;
+// }
+
+// console.log(calAge1(2001))
+
+
+// Function declaration
+// function calAge1(birthyear){
+//     return 2030 - birthyear;
+// }
+
+// console.log(calAge1(2001));
+
+// const age1 = calAge1(2001);
+// console.log(age1);
+
+// const age2 = calAge1(2002);
+// console.log(age2);
+
+
+// // Function expression
+// const calAge3 = function(birthyear){
+//     return 2037 - birthyear;
+// }
+
+// const age3 = calAge3(1991);
+// console.log(age3)
+
+// Arrow function
+// const calAge2 = birthYear => 2037 - birthYear;
+// const ageMain = calAge2(1991);
+// console.log(ageMain)
+
+
+// const yearRetirement = birthyear => {
+//     const age = 2037 - birthyear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+
+// console.log(yearRetirement(2001));
+
+const yearRetirement = (birthyear, firstName) => {
+    const age = 2025 - birthyear;
+    const retirement = 65 - age;
+    return `${firstName} have time left for retirement is ${retirement} years`;
 }
 
-console.log(calAge1(2001))
+console.log(yearRetirement(2001, 'Rejuan'));
+console.log(yearRetirement(1999, 'Samian'));
