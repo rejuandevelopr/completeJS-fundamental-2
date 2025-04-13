0858e2afbbb470f244ea28fecbb030e65052bc3f
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-13 21:08:06 +0600

    2nd

--- script.js
+++ script.js
@@ -14,10 +14,27 @@
 
 
 
-function fruitProcess(apples, oranges){
-    const juice = `This nice juice made of ${apples} apples & ${oranges} oranges`;
+// function fruitProcess(apples, oranges){
+//     const juice = `This nice juice made of ${apples} apples & ${oranges} oranges`;
+//     return juice;
+// }
+
+// console.log(fruitProcess(2,3))
+// console.log(fruitProcess(6,8))
+
+
+function fruitProcess(apple, oranges){
+    const juice = `This juice is made of ${apple} apples & ${oranges} oranges`
     return juice;
 }
 
-console.log(fruitProcess(2,3))
-console.log(fruitProcess(6,8))
+console.log(fruitProcess(2,2));
+
+
+
+function calAge1(birthyear){
+    const age = 2030 - birthyear;
+    return age;
+}
+
+console.log(calAge1(2001))
