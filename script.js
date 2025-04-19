263855c5cc21b6022cef5a146e87e2a43c0eaad7
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-04-19 23:51:29 +0600

    21th

--- script.js
+++ script.js
@@ -520,11 +520,7 @@ while (rep <= 10) {
 }
 
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-while (dice !== 6) {
-    console.log(`You rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-}
+
 
 // const rejuansArray = [
 //     'Rejuanul',
